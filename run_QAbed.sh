e4a352084038ca867d3e21c2378d941fd7a15d1d
winDy <winDy@windystudio.com>
2021-01-22 19:06:20 +0900

    Add run_QAbed.sh

---
+++ run_QAbed.sh
@@ -0,0 +1,33 @@
+if [[ -z $1 ]]; then
+  echo "... No clue file supplied: $1"
+  echo "... Proceed: VPR -> TOKEN -> READ"
+else
+  echo "Clue file supplied..."
+  echo "... Proceed: VPR -> TOKEN -> STORE -> READ"
+fi
+
+# ===========================
+echo ""
+echo "GET Storages from LV-Manager"
+lv-tool vpr -e lv-manager.iconscare.com -o storages.json
+cat storages.json
+echo ""
+echo "GET Tokens from LV-Storages"
+lv-tool token -f storages.json -o tokens.json
+cat tokens.json
+
+if [[ -z $1 ]]; then
+  echo ""
+  echo "READ clues from LV-Storages"
+  lv-tool read -f tokens.json -o restored_clues.txt
+else
+  echo ""
+  echo "STORE clues to LV-Storages"
+  lv-tool store "$1" -f tokens.json -o store_output.json
+  cat store_output.json
+  echo ""
+  echo "READ clues from LV-Storages"
+  lv-tool read -f store_output.json -o restored_clues.txt
+fi
+cat restored_clues.txt
+
