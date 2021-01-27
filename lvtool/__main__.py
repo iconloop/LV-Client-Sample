c7c2b60d7ba148b111c5c8694d6b61c1e38d2c6d
BK winDy Sohn <winDy@windystudio.com>
2021-01-28 06:32:21 +0900

    [SC-212] Add argv to the main function to make the locust test code.

--- lvtool/__main__.py
+++ lvtool/__main__.py
@@ -1,9 +1,14 @@
-from .handlers import handlers
-from .parsers import init_parsers
+from lvtool.handlers import handlers
+from lvtool.parsers import init_parsers
 
 
-def main():
+def main(argv=None):
+    """lv-tool's main
+
+    :param argv: If None, parse_args uses sys.argv [1 :].
+    :return:
+    """
     parser = init_parsers()
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     return handlers[args.dest](args)
