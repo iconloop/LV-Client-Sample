7148ff2f891341847d1959d79fdfffe82acde2a9
winDy <winDy@windystudio.com>
2021-01-28 12:15:58 +0900

    [SC-212] patch _send of Manager while locust test.

--- locust_files/full_scenario.py
+++ locust_files/full_scenario.py
@@ -1,9 +1,10 @@
-from locust import User, task, between
+from locust import HttpUser, task, between
 
 from lvtool.__main__ import main
+from lvtool.interfaces import Manager
 
 
-class LiteVaultClient(User):
+class LiteVaultClient(HttpUser):
     wait_time = between(1, 2.5)
 
     @task
@@ -13,11 +14,18 @@ class LiteVaultClient(User):
         :return:
         """
         test_argv = ['lv-tool', 'vpr', '-e', 'lv-manager.iconscare.com', '-o', 'storages.json']
-        main(test_argv)
 
-    @task(3)
-    def view_items(self):
-        pass
+        def _send(_self, jwe_token):
+            # print(f'In send jwe_token({jwe_token})')
+            response = self.client.post(f"http://lv-manager.iconscare.com/vault", headers={
+                "Authorization": jwe_token
+            }, name='VPR')
+
+            return response.text[1:-1]  # remove quotes...
+
+        Manager._send = _send
+
+        main(test_argv)
 
     def on_start(self):
         pass
