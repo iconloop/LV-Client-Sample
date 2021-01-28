19f453fc6392a34937650f1bac926ee5d24d5bc5
winDy <winDy@windystudio.com>
2021-01-28 14:16:24 +0900

    [SC-212] Add all test for full scenario.

--- locust_files/full_scenario.py
+++ locust_files/full_scenario.py
@@ -1,23 +1,21 @@
 from locust import HttpUser, task, between
 
 from lvtool.__main__ import main
-from lvtool.interfaces import Manager
+from lvtool.interfaces import Manager, Storage
 
 
 class LiteVaultClient(HttpUser):
     wait_time = between(1, 2.5)
 
-    @task
+    @task(1)
     def vpr(self):
         """Get Storages
 
         :return:
         """
-        test_argv = ['lv-tool', 'vpr', '-e', 'lv-manager.iconscare.com', '-o', 'storages.json']
-
-        def _send(_self, jwe_token):
+        def _send(_self: Manager, jwe_token):
             # print(f'In send jwe_token({jwe_token})')
-            response = self.client.post(f"http://lv-manager.iconscare.com/vault", headers={
+            response = self.client.post(f"{_self._endpoint}/vault", headers={
                 "Authorization": jwe_token
             }, name='VPR')
 
@@ -25,7 +23,66 @@ class LiteVaultClient(HttpUser):
 
         Manager._send = _send
 
-        main(test_argv)
+        main(['lv-tool', 'vpr', '-e', 'lv-manager.iconscare.com', '-o', 'storages.json'])
+
+    @task(2)
+    def token(self):
+        """Get Tokens from Storages
+
+        :return:
+        """
+        def _send(_self: Storage, jwe_token):
+            # print(f'In send jwe_token({jwe_token})')
+            response = self.client.post(f"{_self._endpoint}/vault", headers={
+                "Authorization": jwe_token
+            }, name='TOKEN')
+            assert response.status_code == 200
+            return response.text[1:-1]  # remove quotes...
+
+        Storage._send = _send
+
+        main(['lv-tool', 'token', '-f', 'storages.json', '-o', 'tokens.json'])
+
+    @task(6)
+    def store(self):
+        """Store clues to Storages
+
+        :return:
+        """
+        def _send(_self: Storage, jwe_token):
+            # print(f'In send jwe_token({jwe_token})')
+            response = self.client.post(f"{_self._endpoint}/vault", headers={
+                "Authorization": jwe_token
+            }, name='STORE')
+            assert response.status_code == 200
+            return response.text[1:-1]  # remove quotes...
+
+        Storage._send = _send
+
+        main(['lv-tool', 'store', 'clues', '-f', 'tokens.json', '-o', 'store_output.json'])
+
+    @task(3)
+    def read(self):
+        """Read clues from Storages
+
+        :return:
+        """
+        def _send(_self: Storage, jwe_token):
+            # print(f'In send jwe_token({jwe_token})')
+            response = self.client.post(f"{_self._endpoint}/vault", headers={
+                "Authorization": jwe_token
+            }, name='READ')
+            assert response.status_code == 200
+            return response.text[1:-1]  # remove quotes...
+
+        Storage._send = _send
+
+        main(['lv-tool', 'read', '-f', 'store_output.json', '-o', 'restored_clues.txt'])
 
     def on_start(self):
-        pass
+        """Prepare files
+        """
+        main(['lv-tool', 'vpr', '-e', 'lv-manager.iconscare.com', '-o', 'storages.json'])
+        main(['lv-tool', 'token', '-f', 'storages.json', '-o', 'tokens.json'])
+        main(['lv-tool', 'store', 'clues', '-f', 'tokens.json', '-o', 'store_output.json'])
+        main(['lv-tool', 'read', '-f', 'store_output.json', '-o', 'restored_clues.txt'])
